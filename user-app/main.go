270217ecdf2297ad8cc7c0be61589eab627700a0
Ryuya Ikeda <rikeda71@gmail.com>
2020-11-28 14:05:47 +0900

    add table settings and dev/prod settings

--- user-app/main.go
+++ user-app/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type User struct {
+	gorm.Model
+	Email string `json:"user_id"`
+	Password string `json:"password"`
+}
+
+func gormConnect() *gorm.Migrator {
+	USER := "root"
+	PASS := "mysql"
+	PROTOCOL := "tcp(user-db:3306)"
+	DBNAME := "micro_user"
+
+	CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?charset=utf8&parseTime=true"
+	db, err := gorm.Open(mysql.Open(CONNECT), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	migrator := db.Migrator()
+	if !migrator.HasTable(User{}) {
+		migrator.CreateTable(User{})
+	}
+
+	return &migrator
+}
 
 func main() {
-	fmt.Println("Hello world")
+	gormConnect()
 }
